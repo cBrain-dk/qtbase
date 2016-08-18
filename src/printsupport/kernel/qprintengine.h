wkhtmltopdf/qt

add support for not compressing pdf files

src/gui/painting/qprintengine.h
@@ -90,6 +90,8 @@ public:
         PPK_SupportsMultipleCopies,
         PPK_PaperSize = PPK_PageSize,
 
+        PKK_UseCompression = 0xfe10,
+
         PPK_CustomBase = 0xff00
     };
 

cBrain-dk/qtbase

src/printsupport/kernel/qprintengine.h
@@ -89,6 +89,8 @@ public:
         PPK_QPageMargins,
         PPK_QPageLayout,
         PPK_PaperSize = PPK_PageSize,
+        
+        PPK_UseCompression = 0xfe10,
 
         PPK_CustomBase = 0xff00
     };